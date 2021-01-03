995feb6fef0a16f8153442355266c4b02c3c28ce
Mikko Malkavaara <mikko.malkkis@gmail.com>
2021-01-03 14:45:27 +0200

    first commit

---
+++ src/test/java/net/malkkis/api/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.malkkis.api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
